14d7b409d29f1ceb7abfd4004eb73cad84fc668c
Sven Wesley <sven.wesley@cygni.se>
2017-11-07 13:35:37 +0100

    Added post hook to clean up tmp files when C* is down

--- src/main/groovy/se/primenta/common/persistence/test/CassandraTestBase.groovy
+++ src/main/groovy/se/primenta/common/persistence/test/CassandraTestBase.groovy
@@ -1,20 +1,24 @@
 package se.primenta.common.persistence.test
 
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
+import java.time.Instant
 
+import org.apache.cassandra.service.StorageService
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper
 
 class CassandraTestBase {
 
-    static int cassandraPort = 0;
+    static int cassandraPort = 0
 
     static {
 
-        cassandraPort = PortUtil.getNextAvailable();
-        def content = this.getClass().getResourceAsStream('/utils-test-cassandra.yml').text;
-        content = content.replaceAll('__PORT__', String.valueOf(cassandraPort));
-        content = content.replaceAll('__DIR__', String.valueOf(getStartDir()));
+        Path tmpPath = Files.createTempDirectory('embeddedcassandra_' + Instant.now().toEpochMilli())
+
+        cassandraPort = PortUtil.getNextAvailable()
+        def content = this.getClass().getResourceAsStream('/utils-test-cassandra.yml').text
+        content = content.replaceAll('__PORT__', String.valueOf(cassandraPort))
+        content = content.replaceAll('__DIR__', tmpPath.toString())
 
         File file = File.createTempFile('cassandra', '.yaml').with {
             deleteOnExit()
@@ -22,10 +26,10 @@ class CassandraTestBase {
             return it
         }
 
-        EmbeddedCassandraServerHelper.startEmbeddedCassandra(file, System.properties.'java.io.tmpdir'+'/668', 20000L);
-    }
+        EmbeddedCassandraServerHelper.startEmbeddedCassandra(file, tmpPath.toString(), 20000L)
+        // Unfortunately we have no way to check if C* is up and temp
+        // files must be deleted last or we will get stack traces.
+        StorageService.instance.addPostShutdownHook([run: { tmpPath.toFile().deleteDir() }] as Runnable)
 
-    static def getStartDir() {
-        Files.exists(Paths.get('build'))  ? 'build' : Files.exists(Paths.get('target')) ? 'target' : 'tmp'
     }
 }
